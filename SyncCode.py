47453ee1890e1ca96753f96b6af81d41d196cb2c
YoussefAlsoufi <youssefalsoufi.1@gmail.com>
2023-02-09 00:41:54 +0100

    Add a report file to print all steps

--- SyncCode.py
+++ SyncCode.py
@@ -4,12 +4,17 @@ import shutil
 import hashlib
 
 info_log=[]
+path = '/Users/Josef/Projects/SycnTwoFolderWithPython/'
+
 def RewriteFile(srcPath,desPath,file,message):
     try:
         shutil.copy(srcPath, desPath)
         info_log.append( message +" "+file+" is done successfully in "+desPath)
+        #WriteToTxtFile(message +" "+file+" is done successfully in "+desPath)
+        
     except:
         info_log.append( message + file + " in "+desPath+ "didn't happend with an exception, please check again!")
+        #WriteToTxtFile(message + file + " in "+desPath+ "didn't happend with an exception, please check again!")
         
 def CalculateMd5(srcFilePath):
     hasher=hashlib.md5()
@@ -18,9 +23,30 @@ def CalculateMd5(srcFilePath):
         hasher.update(content) 
     return hasher.hexdigest()
 
+def WriteToTxtFile ():
+    comparison = filecmp.dircmp('/Users/Josef/Projects/SycnTwoFolderWithPython/SourceFolder', '/Users/Josef/Projects/SycnTwoFolderWithPython/ReplicaFolder')
+    sourcefolderfiles = ','.join(comparison.left_only)
+    replicaFolderFiles =','.join(comparison.right_only)
+    
+    common_files = ','.join(comparison.common)
+    
+    with open(path+ 'Report.txt','w' ) as Folder_report:
+        Folder_report.write("Steps of synchronising the folders.\n\n")
+        for line in info_log:
+            Folder_report.write(line)
+            Folder_report.write("\n\n")
+        Folder_report.write("\n")
+        Folder_report.write("="*25+"\n")
+        Folder_report.write("Report To check Unmatched Folders/Files.\n\n")
+        Folder_report.write("Common Files : \n" + common_files+ "\n")
+        Folder_report.write("\n Source File contains : \n" + sourcefolderfiles + "\n")
+        Folder_report.write("\n Replica Folder contains :\n" + replicaFolderFiles + "\n")
+    
+    
 # This Check Fun will check the Content of the file after Copying/Creating to confirm that no changing after the process is done.
 def CheckContent(srcFilePath,desFilePath,file, message):
     if (CalculateMd5(srcFilePath)!= CalculateMd5(desFilePath+'/'+file)):
+        #WriteToTxtFile("The Content of "+file+" has been changing by someone'Hacker' during "+ message+ " the file, ReWrite the file in progress.")
         info_log.append("The Content of "+file+" has been changing by someone'Hacker' during "+ message+ " the file, ReWrite the file in progress.")
         RewriteFile(srcFilePath, desFilePath, file, message)
 
@@ -34,6 +60,7 @@ def RemoveUnmatchedFiles(desPath, srcPath):
             if not (os.path.exists(srcFilePath)):
                 os.remove(desFilePath)
                 info_log.append(item +" Removed from "+ folders)
+                #WriteToTxtFile(item +" Removed from "+ folders)
             
 def RemoveUnmatchedDirs(desPath, srcPath):    
     for folders,subFolders,items in os.walk(desPath):
@@ -45,24 +72,27 @@ def RemoveUnmatchedDirs(desPath, srcPath):
                 else:
                     shutil.rmtree(folders+'/'+folder)
                     info_log.append(folder +" Removed from "+ folders)
+                    #WriteToTxtFile(folder +" Removed from "+ folders)
             except FileNotFoundError as e:
                     info_log.append("An Error with removing "+ folders+" "+e)
+                    #WriteToTxtFile("An Error with removing "+ folders+" "+e)
                 
-            
-    
-#path = '/Users/Josef/Projects/SycnTwoFolderWithPython/'
+        
 
             
 # Copy the content of the first file in            
 def SelectItems(srcPath,desPath):
+    info_log.clear()  
     for dirs,subdirs,files in os.walk(srcPath):
         for subdir in subdirs :
             desSubfolderPathReplaced=dirs.replace(srcPath,desPath)
             try:
                 os.mkdir(desSubfolderPathReplaced+'/'+subdir)
                 info_log.append("The "+subdir+" is Created in "+desSubfolderPathReplaced)
+                #WriteToTxtFile("The "+subdir+" is Created in "+desSubfolderPathReplaced)
             except :
                 info_log.append("The Folder "+subdir+" is already in "+desSubfolderPathReplaced)
+                #WriteToTxtFile("The Folder "+subdir+" is already in "+desSubfolderPathReplaced)
           
         for file in files :
             desFilePathReplaced= dirs.replace(srcPath,desPath)
@@ -71,11 +101,13 @@ def SelectItems(srcPath,desPath):
             if (os.path.exists(desFilePath)):
                 if (CalculateMd5(srcFilePath)!= CalculateMd5(desFilePath)):
                     info_log.append("The Content of "+ file+" has changed in the SourceFolder so it's going to Copy the updates.")
+                    #WriteToTxtFile("The Content of "+ file+" has changed in the SourceFolder so it's going to Copy the updates.")
                     RewriteFile(srcFilePath, desFilePathReplaced,file,"Copying")
                     CheckContent(srcFilePath, desFilePathReplaced, file, "Copying")
                     
                 else:
                     info_log.append(file+" is already in "+ desFilePathReplaced +" with all contents")
+                    #WriteToTxtFile(file+" is already in "+ desFilePathReplaced +" with all contents")
                     CheckContent(srcFilePath, desFilePathReplaced, file, "Copying")
             else:
                 RewriteFile(srcFilePath, desFilePathReplaced,file,"Creating")
@@ -83,33 +115,37 @@ def SelectItems(srcPath,desPath):
                 
     RemoveUnmatchedFiles(desPath,srcPath)
     RemoveUnmatchedDirs(desPath,srcPath)
-                               
+    WriteToTxtFile()
+
     
-info_log.clear()  
+
 SelectItems('/Users/Josef/Projects/SycnTwoFolderWithPython/SourceFolder', '/Users/Josef/Projects/SycnTwoFolderWithPython/ReplicaFolder')
 
 
 
-
+'''
 # Compare the content of two folders :
 
 comparison = filecmp.dircmp('/Users/Josef/Projects/SycnTwoFolderWithPython/SourceFolder', '/Users/Josef/Projects/SycnTwoFolderWithPython/ReplicaFolder')
 comparisonFull=comparison.report_partial_closure()
 
 
-with open('/Users/Josef/Desktop/folder_info.txt','w' ) as Folder_report:
-    print("Report : ",comparisonFull,file=Folder_report)
-'''
+sourcefolderfiles = ','.join(comparison.left_only)
+replicaFolderFiles =','.join(comparison.right_only)
+
 common_files = ','.join(comparison.common)
 
-sourcefolderfiles = ','.join(comparisonFull.left_only)
-replicaFolderFiles =','.join(comparisonFull.right_only)
-
 with open(path+ 'folder_info.txt','w' ) as Folder_report:
+    Folder_report.write("Steps of synchronising the folders.\n\n")
+    for line in info_log:
+        Folder_report.write(line)
+        Folder_report.write("\n\n")
+    Folder_report.write("\n")
+    Folder_report.write("="*25+"\n")
+    Folder_report.write("Report To check Unmatched Folders/Files.\n")
     Folder_report.write("Common Files : \n" + common_files+ "\n")
     Folder_report.write("\n Source File contains : \n" + sourcefolderfiles + "\n")
     Folder_report.write("\n Replica Folder contains :\n" + replicaFolderFiles + "\n")
-
 '''
 
 
